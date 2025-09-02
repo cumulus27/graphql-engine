dcefcd6192d5737d0734a0ce57b5f106e471cba9
py <zhaopy27@outlook.com>
2025-09-02 11:35:08 +0800

    build(cli-ext): 更新pkg目标平台到node22版本
    将构建脚本中的pkg目标平台从node16更新到node22，以支持最新的Node.js版本

--- cli-ext/build.sh
+++ cli-ext/build.sh
@@ -8,7 +8,7 @@ export NODE_OPTIONS="--max-old-space-size=13312"
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
 rm -rf ./bin/*
-pkg ./build/command.js --output ./bin/cli-ext -t node16-linux-x64,node16-macos-x64,node16-win-x64,node16-linux-arm64,node16-macos-arm64
+pkg ./build/command.js --output ./bin/cli-ext -t node22-linux-x64,node22-macos-x64,node22-win-x64,node22-linux-arm64,node22-macos-arm64
 
 for binary in bin/cli-ext-*; do
   if ! [[ "$binary" =~ \.sha256$ ]]; then
